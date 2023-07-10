577ddccddc094516b541b3f782d99f203f735da9
Harshit  Satyanarayan Prasad <king16harshit@gmail.com>
2023-07-10 20:34:23 +0530

    Add files via upload

---
+++ hook/useFetch.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import { RAPID_API_KEY } from '@env';
+
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      // "X-RapidAPI-Key": rapidApiKey ,
+      "X-RapidAPI-Key":"a87cf843f4mshdd1c0b23a6022f8p160ad6jsne3286e4a1724",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
